5d1a92ecf5f2d07db9238df566189faf0187a4f4
Enrico Giurin <enricogiurin@gmail.com>
2025-08-10 23:29:32 +0200

    basic keycloak configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { includeBearerTokenInterceptor } from 'keycloak-angular';
 import { provideRouter } from '@angular/router';
 
+import { provideKeycloakAngular } from './keycloak.config';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideKeycloakAngular(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([includeBearerTokenInterceptor]))
   ]
 };
