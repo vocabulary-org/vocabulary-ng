b736e8c5d073297590230697d2984b2186cbc143
Enrico Giurin <enricogiurin@gmail.com>
2025-08-25 01:53:26 +0200

    fully working version -  layout to improve

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { WordListComponent } from './word/word-list/word-list.component';
 
 export const routes: Routes = [
-  { path: '', component: HeaderComponent },
+
   {
     path: 'words',
     component: WordListComponent,
