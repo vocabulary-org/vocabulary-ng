09789632f91c70740040c714cd134808b03c9636
Enrico Giurin <enricogiurin@gmail.com>
2025-08-26 02:41:54 +0200

    basic css and creation add word component

---
+++ src/app/word/word-create/word-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-create',
+  imports: [],
+  templateUrl: './word-create.component.html',
+  styleUrl: './word-create.component.css'
+})
+export class WordCreateComponent {
+
+}
