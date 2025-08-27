f4ab4f70c4d85c17d6aaf221e3a6527b35fe8cf0
Enrico Giurin <enricogiurin@gmail.com>
2025-08-27 22:06:50 +0200

    basic draft of form to create a new word

--- src/app/word/word-create/word-create.component.ts
+++ src/app/word/word-create/word-create.component.ts
@@ -1,31 +1,47 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-word-create',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './word-create.component.html',
-  styleUrl: './word-create.component.css'
+  styleUrl: './word-create.component.css',
 })
 export class WordCreateComponent {
   form = new FormGroup({
-    email: new FormControl('', {
-      validators: [ Validators.email, Validators.required]
-
+    sentence: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(2)],
     }),
-    password: new FormControl('', {
-    validators: [Validators.required, Validators.minLength(6)]
-    })
-
+    translation: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(2)],
+    }),
   });
 
   onSubmit() {
-    const enteredEmail = this.form.value.email;
-    const enteredPassword = this.form.value.password;
-    console.log(enteredEmail+" - "+enteredPassword);
-
-
+    const enteredSentence = this.form.value.sentence;
+    const enteredTranslation = this.form.value.translation;
+    console.log(enteredSentence + ' - ' + enteredTranslation);
   }
 
+  get sentenceIsInvalid() {
+    return (
+      this.form.controls.sentence.touched &&
+      this.form.controls.sentence.dirty &&
+      this.form.controls.sentence.invalid
+    );
+  }
+
+  get translationIsInvalid() {
+    return (
+      this.form.controls.translation.touched &&
+      this.form.controls.translation.dirty &&
+      this.form.controls.translation.invalid
+    );
+  }
 }
