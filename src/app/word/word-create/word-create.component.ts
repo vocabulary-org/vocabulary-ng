f1b748c8586aa582269b2eac6656e7847079466d
Enrico Giurin <enricogiurin@gmail.com>
2025-08-27 02:01:16 +0200

    draft form

--- src/app/word/word-create/word-create.component.ts
+++ src/app/word/word-create/word-create.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-word-create',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './word-create.component.html',
   styleUrl: './word-create.component.css'
 })
 export class WordCreateComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [ Validators.email, Validators.required]
+
+    }),
+    password: new FormControl('', {
+    validators: [Validators.required, Validators.minLength(6)]
+    })
+
+  });
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail+" - "+enteredPassword);
+
+
+  }
 
 }
