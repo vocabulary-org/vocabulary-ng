ffe5a1ed0b4dfb216759bac999ea15cb67c622c8
Enrico Giurin <enricogiurin@gmail.com>
2025-08-29 13:33:40 +0200

    draft word detail

---
+++ src/app/word/word-detail/word-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-detail',
+  imports: [],
+  templateUrl: './word-detail.component.html',
+  styleUrl: './word-detail.component.css'
+})
+export class WordDetailComponent {
+
+}
