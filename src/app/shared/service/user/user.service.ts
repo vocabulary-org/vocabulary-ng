745774c3f20b81e81c6471e407d2f43cdef222e7
Enrico Giurin <enricogiurin@gmail.com>
2025-09-16 23:24:33 +0200

    confirmation messages for user registration

--- src/app/shared/service/user/user.service.ts
+++ src/app/shared/service/user/user.service.ts
@@ -1,18 +1,22 @@
 // user.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../../model/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private apiUrl = 'http://localhost:9090/api/v1/vocabulary/public/users';
 
   constructor(private http: HttpClient) {}
 
-  createUser(user: User): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}`, user);
+  createUser(user: User): Observable<HttpResponse<User>> {
+    return this.http.post<User>(
+      `${this.apiUrl}`,
+      user,
+      { observe: 'response' } // 👈 tells Angular to return the full response
+    );
   }
 }
