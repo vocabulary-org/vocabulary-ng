c864b3716cb6cb51deb8e6116ed78fe92554b3bb
Enrico Giurin <enricogiurin@gmail.com>
2025-10-17 00:02:43 +0200

    fix issue

--- src/environments/environment.azure.ts
+++ src/environments/environment.azure.ts
@@ -3,5 +3,6 @@ export const environment = {
   name: 'azure',
   apiBaseUrl:  'http://20.223.169.151:9090/api/v1/vocabulary',
   keycloakUrl: 'http://20.223.169.151:18081',
-  keycloakUrlPattern: /^(http:\/\/20\.223\.169\.151)(\/.*)?$/i
+  keycloakUrlPattern: /^(http:\/\/20\.223\.169\.151:9090)(\/.*)?$/i
+
 };
