f5ef8bbbedaa7bf63d1d2361cc8e9887ec7f5943
Enrico Giurin <egch@users.noreply.github.com>
2025-10-19 05:52:02 +0200

    env for connecting to remote azure
    * env
    
    * mising file for azure
    
    * fix issue
    
    * ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    
    * Modify build step for Azure Static Web Apps
    
    Updated the build step to use 'npm run build -- --configuration=azure' for deployment.
    
    * Change output location in Azure workflow
    
    Updated output location for the build process.
    
    * Refactor CI/CD workflow for Azure Static Web Apps
    
    Updated build step to use 'npm ci' and modified build command to 'npx ng build --configuration=azure'.
    
    * Update Azure Static Web Apps workflow for main branch
    
    * Delete .github/workflows/azure-static-web-apps-red-desert-078e8c503.yml
    
    * ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    
    * Add NODE_VERSION to Azure Static Web Apps workflow
    
    Added NODE_VERSION environment variable to build job.
    
    * Modify output location for Azure Static Web Apps
    
    Updated output location in the build and deploy job.
    
    * Fix output location in Azure workflow
    
    Updated output location in the build and deploy job.
    
    * Enhance Azure Static Web Apps CI/CD workflow
    
    Updated the CI/CD workflow to include dependency installation and build steps.
    
    * Refactor Azure Static Web Apps workflow configuration
    
    * Update Azure Static Web Apps workflow with OIDC Client
    
    Replaced the installation of OIDC Client with a specific version and added it to the workflow.
    
    * Delete .github/workflows/azure-static-web-apps-blue-ocean-070bb8e03.yml
    
    * ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    
    * Add npm install step to CI/CD workflow
    
    Added a build step to install npm packages before deployment.
    
    * Add NODE_VERSION to Azure Static Web Apps workflow
    
    Added NODE_VERSION environment variable for the build job.
    
    * Modify Azure Static Web Apps workflow for Angular build
    
    Updated the CI/CD workflow to include a build step for Angular with Azure configuration.
    
    * Update Azure Static Web Apps workflow configuration
    
    Added app_build_command to the build and deploy job.
    
    * allowing not ssl
    
    * allowing not ssl from the index
    
    * allowing
    
    * removed meta header

---
+++ src/environments/environment.azure.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  name: 'azure',
+  apiBaseUrl:  'http://20.223.169.151:9090/api/v1/vocabulary',
+  keycloakUrl: 'http://20.223.169.151:18081',
+  keycloakUrlPattern: /^(http:\/\/20\.223\.169\.151:9090)(\/.*)?$/i
+
+};
