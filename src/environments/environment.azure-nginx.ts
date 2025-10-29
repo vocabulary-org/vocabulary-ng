7d6081b5b86984128e404a7a2365db0fa0590546
Enrico Giurin <enricogiurin@gmail.com>
2025-10-29 01:25:49 +0100

    fixing wrong URL

--- src/environments/environment.azure-nginx.ts
+++ src/environments/environment.azure-nginx.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   name: 'azure',
-  apiBaseUrl:  'http://20.223.169.151:80/rest',
+  apiBaseUrl:  'http://20.223.169.151:80/rest/api/v1/vocabulary',
   keycloakUrl: 'http://20.223.169.151:80',
   keycloakUrlPattern: /^(http:\/\/20\.223\.169\.151:80\/rest)(\/.*)?$/i
 
