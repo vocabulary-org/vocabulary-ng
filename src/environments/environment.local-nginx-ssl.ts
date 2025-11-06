6894d94c49942786f40ff23e14ecb4b054071a4d
Enrico Giurin <enricogiurin@gmail.com>
2025-11-07 00:58:05 +0100

    removed useless port 443

--- src/environments/environment.local-nginx-ssl.ts
+++ src/environments/environment.local-nginx-ssl.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   name: 'local-nginx-ssl',
-  apiBaseUrl:  'http://keycloak.local:443/rest/api/v1/vocabulary',
-  keycloakUrl: 'http://keycloak.local:443',
-  keycloakUrlPattern: /^(?:http:\/\/keycloak\.local:443\/|\/)rest(\/.*)?$/i
+  apiBaseUrl:  'https://keycloak.local/rest/api/v1/vocabulary',
+  keycloakUrl: 'https://keycloak.local',
+  keycloakUrlPattern: /^(?:https:\/\/keycloak\.local\/|\/)rest(\/.*)?$/i
 };
