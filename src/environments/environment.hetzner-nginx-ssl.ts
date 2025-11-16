26da62dfe0fbeb96c6b55e75ba2814c7809932df
Enrico Giurin <enricogiurin@gmail.com>
2025-11-16 22:55:02 +0100

    using Hetzner instead of Azure

---
+++ src/environments/environment.hetzner-nginx-ssl.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  name: 'hetzner-nginx-ssl',
+  apiBaseUrl:  'https://116.203.230.142/rest/api/v1/vocabulary',
+  keycloakUrl: 'https://116.203.230.142',
+  keycloakUrlPattern: /^(https:\/\/116\.203\.230\.142\/rest)(\/.*)?$/i
+
+};
