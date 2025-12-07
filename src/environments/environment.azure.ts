5b0e2f2393b129c10892faf3df83b2f8ffcb8a4f
Enrico Giurin <enricogiurin@gmail.com>
2025-12-07 23:49:45 +0100

    removed azure env

--- src/environments/environment.azure.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  name: 'azure',
-  apiBaseUrl:  'http://20.223.169.151:9090/api/v1/vocabulary',
-  keycloakUrl: 'http://20.223.169.151:18081',
-  keycloakUrlPattern: /^(http:\/\/20\.223\.169\.151:9090)(\/.*)?$/i
-
-};
