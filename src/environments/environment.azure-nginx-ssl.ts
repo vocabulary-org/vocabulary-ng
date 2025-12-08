78a1b781eebc1905c71df1263abd181cd4e7f258
Enrico Giurin <egch@users.noreply.github.com>
2025-12-08 12:04:08 +0100

    17 complete crud operations for words (#30) - list
    * removed azure env
    
    * minimal layout list

--- src/environments/environment.azure-nginx-ssl.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  name: 'azure-nginx-ssl',
-  apiBaseUrl:  'https://20.223.169.151/rest/api/v1/vocabulary',
-  keycloakUrl: 'https://20.223.169.151',
-  keycloakUrlPattern: /^(https:\/\/20\.223\.169\.151\/rest)(\/.*)?$/i
-
-};
--- src/environments/environment.azure-nginx.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  name: 'azure',
-  apiBaseUrl:  'http://20.223.169.151:80/rest/api/v1/vocabulary',
-  keycloakUrl: 'http://20.223.169.151:80',
-  keycloakUrlPattern: /^(http:\/\/20\.223\.169\.151:80\/rest)(\/.*)?$/i
-
-};
