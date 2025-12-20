48c8aa5d64c36e4bd5838733132ef0a0395c3d8f
Enrico Giurin <egch@users.noreply.github.com>
2025-12-20 16:40:39 +0100

    add disclaimer (#37)

---
+++ src/app/disclaimer/disclaimer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disclaimer',
+  imports: [],
+  templateUrl: './disclaimer.component.html',
+  styleUrl: './disclaimer.component.css'
+})
+export class DisclaimerComponent {
+
+}
