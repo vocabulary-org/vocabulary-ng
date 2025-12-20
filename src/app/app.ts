fad33d8d514edb8623e8a8ee79f3293b77d583ab
Enrico Giurin <enricogiurin@gmail.com>
2025-12-20 16:17:33 +0100

    add disclaimer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { DisclaimerComponent } from "./disclaimer/disclaimer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, DisclaimerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
